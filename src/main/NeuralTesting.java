f152f0e4238b60e7400d9ae3abf0e5f82735f9a1
Siretu <erikihr@gmail.com>
2015-02-19 22:12:47 +0100

    Added history file.

--- src/main/NeuralTesting.java
+++ src/main/NeuralTesting.java
@@ -6,29 +6,12 @@ import java.util.HashMap;
 //import genetic.Evolution;
 //import genetic.Population;
 import neuralnetwork.*;
+import environment.*;
 
 public class NeuralTesting {
 
 	public static void main(String[] args) throws InvalidInputLayerException {
-		HashMap<ArrayList<Integer>,ArrayList<Integer>> output = new HashMap<ArrayList<Integer>,ArrayList<Integer>>();
-		ArrayList<Integer> key = new ArrayList<Integer>();
-		ArrayList<Integer> value = new ArrayList<Integer>();
-		key.add(1);
-		key.add(1);
-		key.add(1);
-		key.add(0);
-		key.add(0);
-		key.add(1);
-		key.add(0);
-		key.add(0);
-		key.add(1);
-		value.add(1);
-		output.put(key, value);
-		
-		//Population pop = new Population(9, 7, 1);
-		//Evolution foo = new Evolution(output,pop);
-		//foo.run();
-
+		World w = new World();
 	}
 
 }
