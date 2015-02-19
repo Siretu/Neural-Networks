a7bffd2f8eebd17ff2b7ff5b31d10a53e3ce0289
Siretu <erikihr@gmail.com>
2015-02-19 22:36:26 +0100

    Added method to parse history file to double[]. Also fixed bug in World's constructor.

--- src/main/NeuralTesting.java
+++ src/main/NeuralTesting.java
@@ -1,7 +1,13 @@
 package main;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Scanner;
+
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
 
 //import genetic.Evolution;
 //import genetic.Population;
@@ -12,6 +18,28 @@ public class NeuralTesting {
 
 	public static void main(String[] args) throws InvalidInputLayerException {
 		World w = new World();
+		System.out.println(Arrays.toString(parseHistory()));
+	}
+	
+	public static double[] parseHistory() {
+		ArrayList<Double> result = new ArrayList<Double>();
+		try {
+			Scanner in = new Scanner(new FileReader("history"));
+			while (in.hasNextLine()){
+				String line = in.nextLine();
+				String[] segments = line.split(",");
+				result.add(Double.parseDouble(segments[2]));
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		double[] result2 = new double[result.size()];
+		for (int i = 0; i < result.size(); i++) {
+			result2[i] = result.get(i);
+		}
+		return result2;
+		
 	}
 
 }
