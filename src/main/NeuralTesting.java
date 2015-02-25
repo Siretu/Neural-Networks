24204830af159b149a91e87b16920501277df0d7
Raindirve <shcmd@yahoo.com>
2015-02-25 15:36:22 +0100

    closed file reader IO leak; minor readability changes.

--- src/main/NeuralTesting.java
+++ src/main/NeuralTesting.java
@@ -15,23 +15,27 @@ import neuralnetwork.*;
 import environment.*;
 
 public class NeuralTesting {
+	private static final String HISTORY_FILENAME = "history";
 
 	public static void main(String[] args) throws InvalidInputLayerException {
 		World w = new World();
 		System.out.println(Arrays.toString(parseHistory()));
+		//TODO: Neural Testing
 	}
 	
 	public static double[] parseHistory() {
 		ArrayList<Double> result = new ArrayList<Double>();
 		try {
-			Scanner in = new Scanner(new FileReader("history"));
+			Scanner in = new Scanner(new FileReader(HISTORY_FILENAME));
 			while (in.hasNextLine()){
 				String line = in.nextLine();
 				String[] segments = line.split(",");
 				result.add(Double.parseDouble(segments[2]));
 			}
+			in.close();
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
+			// TODO: Handle file not found
+			System.err.println("File not found: "+HISTORY_FILENAME);
 			e.printStackTrace();
 		}
 		double[] result2 = new double[result.size()];
